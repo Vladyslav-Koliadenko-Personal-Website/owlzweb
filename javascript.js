f3d6cdda5d9a4c4eff683e64000bd7e9a089d4a6
Vlad K <126846256+owlzzzx3@users.noreply.github.com>
2023-04-06 13:56:00 -0700

    UPD. V2
    added portfolio and about pages and linked them to index html, now all of the buttons work properly.
    //need to work on external .js file

---
+++ javascript.js
@@ -0,0 +1,11 @@
+//I was trying to make it work though js file but couldn't figure out why it's not working due to lack of time.. so for now I just made it work thru html files (temporarily)
+//To connect it to html file I used this command     <script src="javascript.js"></script>
+
+/*var navLinks = document.getElementById("navLinks");
+    function showMenu(){
+        navLinks.style.right = "0"
+    }
+    function hideMenu(){
+        navLinks.style.right = "-200px"
+    }
+*/
